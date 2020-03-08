3b5ffc8bb26f6e07766697ddebf01bb689151897
zingz0r <zingz0r@tothnet.hu>
2020-03-08 17:32:01 +0100

    qq

--- loadtests/fail-jenkins-test.js
+++ loadtests/fail-jenkins-test.js
@@ -1,7 +1,10 @@
 import http from 'k6/http'; 
 import { check } from "k6";
 
-export let options = { maxRedirects: 4, vus: 10, duration: '10s' };
+export let options = { maxRedirects: 4, vus: 10, duration: '10s',
+thresholds: {
+  http_req_duration: ["p(95)<1"]
+} };
 
 export default function() {
   let res = http.get("http://test.k6.io/");
